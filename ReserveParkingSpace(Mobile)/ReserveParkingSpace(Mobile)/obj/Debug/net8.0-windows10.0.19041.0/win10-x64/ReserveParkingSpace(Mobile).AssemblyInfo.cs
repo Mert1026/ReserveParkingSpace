8811759eb173fab8bf3ade581c97836a5106e891
Kaloyan3D <124395491+Kaloyan3D@users.noreply.github.com>
2025-07-03 13:18:11 +0300

    Updated frontend

--- ReserveParkingSpace(Mobile)/ReserveParkingSpace(Mobile)/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ReserveParkingSpace(Mobile).AssemblyInfo.cs
+++ ReserveParkingSpace(Mobile)/ReserveParkingSpace(Mobile)/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ReserveParkingSpace(Mobile).AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReserveParkingSpace(Mobile)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+48ac6609a261a91afc33b56fb6578e352ffc456f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6df2f2b140f7291fff0684b865105ee11eeeb103")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReserveParkingSpace(Mobile)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReserveParkingSpace(Mobile)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
